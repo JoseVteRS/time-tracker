eee5c644df49691f4763b646ab62527c25bb102e
Jose <jvrs.90@gmail.com>
2023-07-30 12:20:59 +0200

    feat: Add next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        })
+    ],
+    // callbacks: {
+    //     async signIn({ user, account, profile, email, credentials }) {
+    //         return true;
+    //     },
+    // }
+}
+
+
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
